12737c7efbb17265c39a0cddacc13f6c9c82bdbf
Max <maxlerebourg@gmail.com>
2018-01-01 20:57:31 +0100

    Fini

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,37 +3,48 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+import { AddPage } from '../pages/add/add';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { UpdatePage } from "../pages/update/update";
+import { ItemPage } from "../pages/item/item";
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {ServiceProvider} from "../providers/service";
+import {IonicImageLoader} from "ionic-image-loader";
+import {HttpClientModule} from "@angular/common/http";
+
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    AddPage,
+    ItemPage,
+    UpdatePage,
     HomePage,
     TabsPage
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    IonicImageLoader.forRoot(),
+    IonicImageLoader,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    ItemPage,
+    AddPage,
     HomePage,
+    UpdatePage,
     TabsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    ServiceProvider,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,47 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component, NgModule} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {ServiceProvider} from "../../providers/service";
+import {advert} from "../../class/advert";
+import {IonicImageLoader} from "ionic-image-loader";
+import {UpdatePage} from "../update/update";
+import {ItemPage} from "../item/item";
 
+@NgModule({
+    imports: [
+  IonicImageLoader
+]
+})
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  list: advert[];
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private psp: ServiceProvider) {
+    this.load();
   }
-
+  load(){
+    this.psp.getAdverts()
+    .subscribe(data => {
+      console.log(data);
+      this.list = data;
+      console.log("got it");
+    },err => {
+      console.log(err)
+    });};
+  delete(id){
+    this.psp.removeAdvert(id).subscribe(
+      resp => console.log('deleted'),
+      error => console.log('error occur, delete fail')
+    );
+    console.log("remove "+id);
+  }
+  update(id){
+    this.navCtrl.push(UpdatePage,{id: id});
+    console.log('update');
+  }
+  advert(id){
+    this.navCtrl.push(ItemPage,{id: id});
+  }
 }
